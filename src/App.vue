affc5f2fd75462e55532adff73df159a4a776ba1
Hannes <kontakt@mindt.net>
2022-03-12 16:23:30 +0100

    Confetti with dummy values

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,157 @@
-<script setup>
-import Confetti from "./components/Confetti.vue";
-</script>
-
 <template>
   <div class="full-viewport">
-    <Confetti />
+    <div class="dummy-panel">
+      <div class="dummy-row">
+        <label>itemAmount:</label>
+        <input
+          type="number"
+          v-model="config.itemAmount"
+          @change="changeAmount"
+        />
+      </div>
+      <div class="dummy-row">
+        <label>next:</label>
+        <input type="number" v-model="config.next" />
+      </div>
+      <div class="dummy-row">
+        <label>duration min:</label>
+        <input type="number" v-model="config.duration.min" />
+      </div>
+      <div class="dummy-row">
+        <label>duration max:</label>
+        <input type="number" v-model="config.duration.max" />
+      </div>
+
+      <div class="dummy-row">
+        <div>
+          <label>colors:</label>
+          <div v-for="(color, index) in dummyColors" :key="index">
+            <input
+              type="checkbox"
+              checked
+              @change="toggleColor"
+              :value="color"
+            />
+            {{ color }}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        in ♥️ with <a href="https://motion.dev">motion.dev</a>
+      </div>
+    </div>
+    <Confetti :config="config" v-if="!isReloading" />
   </div>
 </template>
 
+<script setup>
+import { ref } from "vue";
+import Confetti from "./components/Confetti.vue";
+
+const isReloading = ref(false);
+
+const dummyColors = ["purple", "black", "red", "green", "orange", "yellow"];
+const dummyStyles = [
+  {
+    width: "8px",
+    height: "24px",
+  },
+  {
+    width: "12px",
+    height: "12px",
+    borderRadius: "12px",
+  },
+  {
+    width: "14px",
+    height: "14px",
+  },
+];
+
+const config = ref({
+  itemAmount: 24,
+  gap: 50,
+  next: 0.5,
+  duration: {
+    min: 4,
+    max: 6,
+  },
+  colors: [...dummyColors],
+  styles: [...dummyStyles],
+});
+
+function toggleColor({ target }) {
+  const index = config.value.colors.findIndex((c) => c === target.defaultValue);
+  if (!target.checked) {
+    if (index >= 0) config.value.colors.splice(index, 1);
+  } else {
+    if (index <= -1) config.value.colors.push(target.defaultValue);
+  }
+}
+
+// TODO
+function toggleStyle({ target }) {
+  console.log("toggleColor", target.defaultValue);
+  console.log("toggleColor", target.checked);
+  const index = config.value.colors.findIndex((c) => c === target.defaultValue);
+  if (!target.checked) {
+    if (index >= 0) config.value.colors.splice(index, 1);
+  } else {
+    if (index <= -1) config.value.colors.push(target.defaultValue);
+  }
+}
+
+function changeAmount() {
+  isReloading.value = true;
+
+  setTimeout(()=> isReloading.value = false, 200);
+}
+</script>
+
 <style>
-@import "./assets/base.css";
+html, body {
+  margin: 0;
+}
+
+.full-viewport {
+  height: 100vh;
+  width: 100vw;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+a {
+  color: inherit;
+  opacity: 0.6;
+}
+a:hover {
+  opacity: 1;
+}
+
+.dummy-panel {
+  position: relative;
+  top: 30vh;
+  z-index: 10;
+  padding: 20px;
+  margin: auto;
+  width: 280px;
+  background: white;
+  border-radius: 10px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+}
+
+.dummy-row {
+  margin-bottom: 10px;
+}
+
+input {
+  width: 60px;
+}
+
+.dummy-row {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
--- src/components/Confetti.vue
+++ src/components/Confetti.vue
@@ -1,14 +1,154 @@
 <template>
-  <div class="confetti"></div>
+  <div class="confetti">
+    {{ viewWidth }} x {{ viewHeight }}
+    <Motion
+      v-for="item in items"
+      class="confetti-item"
+      :key="item.id"
+      :animate="item.animate"
+      :transition="item.transition"
+      :style="{ ...item.style, background: item.color }"
+    />
+  </div>
 </template>
 
 <script setup>
+import { Motion } from "motion/vue";
+import { onMounted, ref } from "vue";
 
+const props = defineProps({
+  config: {
+    type: Object,
+    default: {
+      itemAmount: 10,
+      gap: 50,
+      next: 0.5,
+      duration: {
+        min: 4,
+        max: 6,
+      },
+      colors: ["purple", "black", "red", "green", "orange", "yellow"],
+      styles: [
+        {
+          width: "8px",
+          height: "20px",
+        },
+        {
+          width: "12px",
+          height: "12px",
+          borderRadius: "12px",
+        },
+        {
+          width: "14px",
+          height: "14px",
+        },
+      ],
+    },
+  },
+});
+
+const viewWidth = ref(0);
+const viewHeight = ref(0);
+
+function initSize() {
+  let box = document.querySelector(".confetti");
+  viewHeight.value = box.offsetHeight;
+  viewWidth.value = box.offsetWidth;
+}
+
+const items = ref([]);
+
+function getRandomInt({ min, max }) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColor() {
+  const colorIndex =
+    Math.floor(Math.random() * props.config.colors.length) || 0;
+  return props.config.colors[colorIndex];
+}
+
+function getRandomStyle() {
+  const styleIndex =
+    Math.floor(Math.random() * props.config.styles.length) || 0;
+  const style = props.config.styles[styleIndex];
+  return style;
+}
+
+function createItems() {
+  items.value = [];
+  for (let i = 0; i <= props.config.itemAmount; i++) {
+    createItem(i);
+  }
+}
+
+function createItem(i) {
+  const duration = getRandomInt(props.config.duration);
+  const easing = "ease-in";
+
+  const id = `item-${i}`;
+
+  const item = {
+    id,
+    style: getRandomStyle(),
+    color: getRandomColor(),
+    animate: {
+      x: [
+        getRandomInt({ min: 0, max: viewWidth.value }),
+        getRandomInt({ min: 0, max: viewWidth.value }),
+      ],
+      y: [
+        getRandomInt({ min: props.config.gap * -1, max: -100 }),
+        viewHeight.value + props.config.gap,
+      ],
+      rotate: [
+        getRandomInt({ min: -180, max: 180 }),
+        getRandomInt({ min: -180, max: 180 }),
+      ],
+    },
+    transition: { duration, easing },
+    exit: { opacity: 0, transition: { duration: 0.8 } },
+  };
+
+  items.value.push(item);
+
+  setTimeout(
+    () => createItem(i + 1),
+    getRandomInt({
+      min: duration * 1000 * props.config.next,
+      max: duration * 1000,
+    }) * 0.5
+  );
+
+  setTimeout(() => {
+    const index = items.value.findIndex((i) => i.id === id);
+    if (index >= 0) items.value.splice(i, 1);
+  }, duration * 1000 + 5000);
+}
+
+onMounted(() => {
+  initSize();
+  createItems();
+});
+
+window.onresize = initSize;
 </script>
 
 <style scoped>
-  .confetti {
-    width: 100%;
-    height: 100%;
-  }
+.confetti {
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.confetti-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
 </style>
